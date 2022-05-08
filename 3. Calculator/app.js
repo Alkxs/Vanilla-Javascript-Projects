dd00117948513220db7e51c559ccf3ff64f8f3c7
Alkxs <72358387+Alkxs@users.noreply.github.com>
2022-05-08 23:59:57 +0200

    JS set up

--- 3. Calculator/app.js
+++ 3. Calculator/app.js
@@ -1,54 +1,45 @@
-const keys = document.querySelector('.keys');
-
-const numbers = document.querySelectorAll('[data-number]');
-const operators = document.querySelectorAll('[data-operation]');
-const decimal = document.querySelector('.decimal');
-const equal = document.querySelector('.equal-sign');
-const clear = document.querySelector('.clear');
-const deleteLast = document.querySelector('.delete');
-
+const calculator = document.querySelector('.calculator');
+const keys = document.querySelectorAll('.keys');
 const screen = document.querySelector('.screen');
 
-keys.addEventListener('click', (e) => {
-  if (!e.target.closest('button')) return;
+const numbers = document.querySelectorAll('[data-type="number"]');
+const operators = document.querySelectorAll('[data-type="operation"]');
+const decimal = document.querySelector('.decimal');
+const equal = document.querySelector('[data-type="equal"]');
+const clear = document.querySelector('[data-type="clear"]');
+const deleteLast = document.querySelector('[data-type="delete"]');
 
-  const key = e.target;
-  const keyValue = key.textContent;
-  const displayValue = screen.textContent;
-  screen.textContent = keyValue;
+// keys.forEach((num) => {
+//   num.addEventListener('click', (e) => {
+//     let key = e.target;
+//     let keyValue = key.textContent;
+//     let displayValue = screen.textContent;
+//     screen.textContent = keyValue;
+//   });
+// });
 
-  if (displayValue === '0') {
-    screen.textContent = keyValue;
-  } else {
-    screen.textContent = displayValue + keyValue;
-  }
+let buttons = Array.from(keys);
 
-  console.log(keyValue);
+buttons.map((key) => {
+  key.addEventListener('click', (e) => {
+    switch (e.target.innerText) {
+      case 'AC':
+        screen.innerText = '';
+        break;
+      case '←':
+        if (screen.innerText) {
+          screen.innerText = screen.innerText.slice(0, -1);
+        }
+        break;
+      case '=':
+        try {
+          screen.innerText = eval(screen.innerText);
+        } catch {
+          screen.innerText = 'Error';
+        }
+        break;
+      default:
+        screen.innerText += e.target.innerText;
+    }
+  });
 });
-
-////testing
-function add(n1, n2) {
-  const sum = n1 + n2;
-  console.log(sum);
-}
-function subtract(n1, n2) {
-  const minus = n1 - n2;
-  console.log(minus);
-}
-function multiply(n1, n2) {
-  const multi = n1 * n2;
-  console.log(multi);
-}
-function divide(n1, n2) {
-  const division = n1 / n2;
-  console.log(division);
-}
-
-function operate() {
-  subtract(10, 2);
-  add(2, 2);
-  multiply(2, 4);
-  divide(10, 2);
-}
-
-operate();
