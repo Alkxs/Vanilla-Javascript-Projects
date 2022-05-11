efae2a4543f35fba112d8429296b7294a2bcee5b
Alkxs <72358387+Alkxs@users.noreply.github.com>
2022-05-11 17:01:25 +0200

    js logic

--- 3. Calculator/app.js
+++ 3. Calculator/app.js
@@ -1,8 +1,8 @@
 const display = document.querySelector('.display');
 const keys = document.querySelectorAll('.keys');
 
-const previous = document.getElementById('previous')
-const current = document.getElementById('current')
+const previous = document.getElementById('previous');
+const current = document.getElementById('current');
 
 const numbers = document.querySelectorAll('[data-type="number"]');
 const operators = document.querySelectorAll('[data-type="operation"]');
@@ -12,52 +12,46 @@ const equal = document.querySelector('[data-type="equal"]');
 const clear = document.querySelector('[data-type="clear"]');
 const deleteLast = document.querySelector('[data-type="delete"]');
 
-keys.forEach((num) => {
-  num.addEventListener('click', (e) => {
-    let key = e.target;
-    let keyValue = key.textContent;
-    let displayValue = display.textContent;
-    display.textContent = keyValue;
+keys.forEach((key) => {
+  key.addEventListener('click', (e) => {
+    let keyPressed = e.target.innerText;
+    let showDisplay = display.innerText;
 
-    switch (keyValue) {
-      case 'AC':
-        display.textContent = '';
-        break;
-      case '←':
-        if (display.textContent) {
-          display.textContent = display.textContent.slice(0, -1);
-        }
-        break;
-      case '=':
-        compute()
+    if (e.target.classList.contains('number') || e.target.classList.contains('operator')) {
+      if (showDisplay === '0' && keyPressed === '0') {
+        display.innerText = '0';
+      } else if (showDisplay === '0') {
+        display.innerText = keyPressed;
+      } else {
+        display.innerText += keyPressed;
+      }
     }
+
+    clear.addEventListener('click', (e) => {
+      display.innerText = '0';
+    });
+
+    // deleteLast.addEventListener('click', (e) => {
+    //   if (display.textContent.length) {
+    //     display.textContent = display.textContent.slice('0', '-1');
+    //   }
+    // });
+
+    // switch (display.innerText) {
+    //   case 'clear':
+    //     display.textContent = '0';
+    //     break;
+    //   case '←':
+    //     if (showDisplay) {
+    //       showDisplay = display.textContent.slice(0, -1);
+    //     }
+    //     break;
+    //   // case '=':
+    //   //   compute();
+    // }
   });
 });
 
-compute(number, op, number2) {
+function compute(number, op, number2) {
+  console.log(number, op, number2);
 }
-
-// let buttons = Array.from(keys);
-// buttons.map((key) => {
-//   key.addEventListener('click', (e) => {
-//     switch (e.target.innerText) {
-//       case 'AC':
-//         display.innerText = '';
-//         break;
-//       case '←':
-//         if (display.innerText) {
-//           display.innerText = display.innerText.slice(0, -1);
-//         }
-//         break;
-//       case '=':
-//         try {
-//           display.innerText = eval(display.innerText);
-//         } catch {
-//           display.innerText = 'Error';
-//         }
-//         break;
-//       default:
-//         display.innerText += e.target.innerText;
-//     }
-//   });
-// });
